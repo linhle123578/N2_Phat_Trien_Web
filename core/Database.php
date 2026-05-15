b1c9e9c997802ca9880dd3254ba272da7e8cb283
lele <linhle.31231023748@st.ueh.edu.vn>
2026-05-15 10:45:21 +0700

    doi lai setup db lan 2

--- core/Database.php
+++ core/Database.php
@@ -4,24 +4,37 @@ class Database {
 
     private $conn;
 
-    public function __construct() {
+    public function connect() {
 
         $config = require __DIR__ . '/../config/database.php';
 
-        $this->conn = new mysqli(
-            $config['host'],
-            $config['user'],
-            $config['pass'],
-            $config['dbname'],
-            $config['port']
+        $parts = parse_url($config['url']);
+
+        $host = $parts['host'];
+        $port = $parts['port'];
+        $user = $parts['user'];
+        $pass = $parts['pass'];
+        $db   = ltrim($parts['path'], '/');
+
+        $this->conn = mysqli_init();
+
+        mysqli_ssl_set($this->conn, NULL, NULL, NULL, NULL, NULL);
+
+        $ok = mysqli_real_connect(
+            $this->conn,
+            $host,
+            $user,
+            $pass,
+            $db,
+            $port,
+            NULL,
+            MYSQLI_CLIENT_SSL
         );
 
-        if ($this->conn->connect_error) {
-            die("Connection failed");
+        if (!$ok) {
+            die("DB connection failed: " . mysqli_connect_error());
         }
-    }
 
-    public function connect() {
         return $this->conn;
     }
 }
