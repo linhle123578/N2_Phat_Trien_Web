a800f2e6bac62e1b2f11b476694ef486034cfaa4
Lê Nguyễn Thuỳ Linh <linhle.31231023748@st.ueh.edu.vn>
2026-05-15 10:54:37 +0700

    Delete config/database.php

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'host' => $_ENV['DB_HOST'],
-    'port' => $_ENV['DB_PORT'],
-    'dbname' => $_ENV['DB_NAME'],
-    'user' => $_ENV['DB_USER'],
-    'pass' => $_ENV['DB_PASS']
-];
