f37c562a1d9c180d7fb22b81835416cfb7011a37
lele <linhle.31231023748@st.ueh.edu.vn>
2026-05-15 10:11:22 +0700

    đổi cấu hình kết nối db

--- tets.php
+++ tets.php
@@ -1,9 +1,16 @@
 <?php
 
-require_once "core/Database.php";
+$conn = new mysqli(
+    "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
+    "3YHrkxqAKWynehu.root",
+    "6AtFiqD6KByTAAm",
+    "db_web_farm2home",
+    4000
+);
 
-$db = new Database();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-$conn = $db->connect();
-
-echo "Kết nối database thành công";
+echo "Connected successfully!";
+?>
