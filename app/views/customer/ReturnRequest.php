661a24820bf3c3e536cb8fa43722c434acdddd0f
phungle31231026280 <phungle.31231026280@st.ueh.edu.vn>
2026-05-30 20:53:35 +0700

    sửa return

--- app/views/customer/ReturnRequest.php
+++ app/views/customer/ReturnRequest.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 ob_start();
 include_once __DIR__ . '/../layouts/header.php';
 
@@ -363,6 +363,7 @@ if (!empty($controller->view_name)) {
         </div>
 
         
+        <?php include_once __DIR__ . '/../layouts/footer.php'; ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         </body>
         </html>
@@ -370,9 +371,3 @@ if (!empty($controller->view_name)) {
     }
 }
 ?>
-
-<<<<<<< HEAD
-
-<?php include_once __DIR__ . '/../layouts/footer.php'; ?>
-=======
->>>>>>> b0de28287d8381b6f88c230b9818ee9e6a08010f
