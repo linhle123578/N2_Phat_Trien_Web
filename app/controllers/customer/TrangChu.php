9aab65cb776ad0f496ebe4aaae7eedb384a0c39e
Huỳnh Bảo Nhi <177523003+nhihuynh31231020861@users.noreply.github.com>
2026-06-01 09:24:12 +0700

    Clean up index method in HomeController
    Removed commented code for featured products.

--- app/controllers/customer/TrangChu.php
+++ app/controllers/customer/TrangChu.php
@@ -6,10 +6,6 @@ require_once "../app/models/TrangChuModel.php";
 class HomeController extends controller {
 
     public function index() {
-
-        // 🔥 chỉ truyền sản phẩm nổi bật
-        //$GLOBALS['featuredProducts'] = $featuredProducts;
-
         $this->view("customer/TrangChu");
     }
 }
